0f5a9b8ea81f6f8308cc7779cc96faa723719b3a
Fikunmi01 <93198530+Fikunmi01@users.noreply.github.com>
2023-05-16 17:39:35 +0100

    Update: Removed declared but not used statement

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Hero } from "./components/hero"
 import { Navbar } from "./components/navbar"
 import { PopularDestination } from "./components/popDestination"
 import { Testimonial } from "./components/testimonial"
-import { DestinationArray, TopDestination } from "./components/topDestinations"
+import { TopDestination } from "./components/topDestinations"
 
 function App() {
 
